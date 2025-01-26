bf70c889179104b7a86353b23114b62b913ad31d
taofeeq2014 <taofeeq2014@outlook.com>
2025-01-27 00:38:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM fully loaded and parsed.");
+});
+
 const form = document.getElementById('registration-form');
 
 // Prevent form submission
